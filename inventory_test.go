28ecce8bba7a5c71c489d626d146b775b7c14f68
Yurii-Korovkin <earn.yurii@gmail.com>
2026-07-14 16:57:26 +0300

    Thread-safe inventory service with RWMutex num.3

---
+++ inventory_test.go
@@ -0,0 +1,36 @@
+package inventory
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestGetStock_Basic(t *testing.T) {
+	
+
+func TestReserve_Basic(t *testing.T) {
+
+
+
+}
+
+
+func TestReserve_ConcurrentOversell(t *testing.T) {
+	
+}
+
+
+func TestReserveMultiple_Atomicity(t *testing.T) {
+	
+}
+
+
+func TestReserveMultiple_ConcurrentAtomicity(t *testing.T) {
+	
+}
+
+
+func TestGetStock_ConcurrentWithReserve(t *testing.T) {
+	
+}
